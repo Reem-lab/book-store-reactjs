1e6439de4993ce254a98be8205683b4a7c45ab4b
Reem <janinareem@gmail.com>
2022-02-22 03:46:54 +0200

    Add routes in our app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Categories from './components/Categories';
+import NavBar from './components/NavBar';
+import Books from './components/Books';
+import './components/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Books />
+        </Route>
+        <Route path="/Categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
