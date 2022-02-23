286c6cf9b397d1547321ad7d84b430dc519ac9ac
Reem <janinareem@gmail.com>
2022-02-23 02:41:20 +0200

    Add redux dispatch to the form to add new book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,37 +1,50 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/book';
 
 const CATEGORIES = ['romantic', 'hisorical', 'geographics', 'physics'];
 const AddBook = () => {
+  const dispatch = useDispatch();
   const [category, setCategory] = useState('');
+  const [title, setTitle] = useState('');
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      category,
+    };
+    dispatch(addBook(newBook));
+  };
 
   return (
     <form
       className="wrapForm"
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
+      onSubmit={submitBookToStore}
     >
-      <h2 className="title">
-        ADD NEW BOOK
-      </h2>
+      <h2 className="title">ADD NEW BOOK</h2>
       <div className="Lesson-Panel ">
-        <input className="inputTitle" placeholder=" Book title" />
+        <input
+          className="inputTitle"
+          placeholder=" Book title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
         <select
           id="category"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
-          onBlur={(e) => setCategory(e.target.value)}
         >
-          {
-                   CATEGORIES.map((category) => (
-                     <option value={category} key={category}>
-                       {category}
-                     </option>
-                   ))
-               }
+          {CATEGORIES.map((category) => (
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </div>
-      <button type="button">ADD BOOK</button>
+      <button type="submit">ADD BOOK</button>
     </form>
   );
 };
