6013f8fe6864b74644c83dda9ebd8fd8ae5a4136
Reem <janinareem@gmail.com>
2022-02-25 19:10:43 +0200

    Import the app.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import Categories from './components/Categories';
 import NavBar from './components/NavBar';
 import Books from './components/Books';
+import './styles/App.css';
 
 function App() {
   return (
